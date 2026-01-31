b9e55f3e66e12680d171ffc27bf4fb7eb58c67bc
tanmaylagoo <tanmaylagoo@gmail.com>
2026-01-31 17:47:54 +0530

    made changes of connecting to db and node --watch

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! vansh kataria');
+  res.send('Hello, World! tanny');
 });
 
-app.listen(PORT, () => {
+connectDB.then(()=>{
+  app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+})
+
